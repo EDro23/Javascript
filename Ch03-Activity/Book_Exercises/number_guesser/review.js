25b21ac80985e14cf9b50eef96d53e3161adb50f
Ethan <Ethandrover@hotmail.com>
2024-09-23 00:43:34 -0230

    Number_Guesser

--- Ch03-Activity/Book_Exercises/number_guesser/review.js
+++ Ch03-Activity/Book_Exercises/number_guesser/review.js
@@ -1,22 +1,42 @@
 "use strict";
 
-const secretNum = []
+function runGame() {
+    let secretNum = Math.floor(Math.random() * 11);
+    let counter = 0;
+    let input;
 
-const counter = 0
-
-do {
-    const input = parseInt(
-        prompt("Enter a number between 1-10"));
-    
-        let secretNum = Math.floor(Math.random * 11);
-
-    if (input == secretNum) {
-        document.write("Correct number!")
-        document.write(`<p>Guesses: ${counter}}</p>`)
+    if (document.getElementById("restartButton")) {
+        document.getElementById("restartButton").remove();
     }
-    if (input != secretNum){
-        alert("Wrong number, Try again!");
-        counter += 1
-    }
+
+    do {
+        input = parseInt(prompt("Enter a number between 1-10"));
+
+        if (isNaN(input)) {
+            alert("Not a number, Please try again.");
+        }
+        else if (input === secretNum) {
+            alert("Correct number!");
+            alert(`It took you ${counter} guesses!`);
+
+            document.write(`Correct number!\n`);
+            document.write(`Guesses: ${counter}`);
+
+            if (!document.getElementById("restartButton")) {
+                document.body.insertAdjacentHTML('beforeend', `<button id="restartButton">Try again</button>`);
+            }
+
+            document.getElementById("restartButton").onclick = function () {
+                runGame();
+            };
+
+            break;
+        }
+        else {
+            alert("Wrong number, Try again!");
+            counter += 1;
+        }
+    } while (input !== secretNum);
 }
-while(secretNum != input);
+
+runGame();
