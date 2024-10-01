9ec0004535f42a83990b936cb89c159ae14e5d88
Ethan <Ethandrover@hotmail.com>
2024-10-01 10:44:23 -0230

    Assignment2_Cleanedup

--- Assignments/Assignment02/sales_arrays/sales.js
+++ Assignments/Assignment02/sales_arrays/sales.js
@@ -13,60 +13,47 @@ let quarterTwo = (region1[1]+region2[1]+region3[1]+region4[1]+region5[1])
 let quarterThree = (region1[2]+region2[2]+region3[2]+region4[2]+region5[2])
 let quarterFour = (region1[3]+region2[3]+region3[3]+region4[3]+region5[3])
 
-// Region 1 total
+// Region 1 total, added to totalSales
 
 let regionOneSum = 0;
+let totalSales = 0;
+
 
 for (let i = 0; i < region1.length; i++) {
     regionOneSum += region1[i];
+    totalSales += region1[i];
 }
-// Region 2 total
+// Region 2 total, added to totalSales
 
 let regionTwoSum = 0;
 
 for (let i = 0; i < region2.length; i++) {
     regionTwoSum += region2[i];
+    totalSales += region2[i];
 }
-// Region 3 total
+// Region 3 total, added to totalSales
 
 let regionThreeSum = 0;
 
 for (let i = 0; i < region3.length; i++) {
     regionThreeSum += region3[i];
+    totalSales += region3[i];
 }
-// Region 4 total
+// Region 4 total, added to totalSales
 
 let regionFourSum = 0;
 
 for (let i = 0; i < region4.length; i++) {
     regionFourSum += region4[i];
+    totalSales += region4[i];
 }
-// Region 5 total
+// Region 5 total, added to totalSales
 
 let regionFiveSum = 0;
 
 for (let i = 0; i < region5.length; i++) {
     regionFiveSum += region5[i];
-}
-
-// Total for all regions for all quarters
-
-let totalSales = 0
-
-for (let i = 0; i < region1.length; i++) {
-    totalSales += region1[i]
-}
-for (let i = 0; i < region2.length; i++) {
-    totalSales += region2[i]
-}
-for (let i = 0; i < region3.length; i++) {
-    totalSales += region3[i]
-}
-for (let i = 0; i < region4.length; i++) {
-    totalSales += region4[i]
-}
-for (let i = 0; i < region5.length; i++) {
-    totalSales += region5[i]
+    totalSales += region5[i];
 }
 
 document.write(`<h3>\nSales by Quarter\n</h3>`)
