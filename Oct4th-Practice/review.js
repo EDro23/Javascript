0dd63a946cb8cc46c11a91a86a14afeae423bd4c
edro23 <ethandrover@hotmail.com>
2024-10-10 12:21:14 -0230

    Oct10...

--- Oct4th-Practice/review.js
+++ Oct4th-Practice/review.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     showDateButton.addEventListener('click', () => {
         const currentDate = new Date();
-        const formattedDate = `${currentDate.getFullYear()}/${String(currentDate.getMonth() + 1)}/${String(currentDate.getDate())}`;
+        const formattedDate = `${currentDate.getFullYear()}**${String(currentDate.getMonth() + 1).padStart(2,"0")}**${String(currentDate.getDate()).padStart(2,"0")}`;
 
         alert(`${formattedDate}`)
         textInput.value = formattedDate;
